e08c88928801e70c280bff618ef45c78f0e81c4b
Kirill Leonov <leonov835@yandex.ru>
2024-04-02 21:31:39 +0300

    init

---
+++ config/puma.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env puma
+# frozen_string_literal: true
+
+threads_count = ENV.fetch('PUMA_THREADS') { 5 }.to_i
+
+threads threads_count, threads_count
+
+port ENV.fetch('PORT') { 3000 }
+
+workers ENV.fetch('WORKERS') { 1 }.to_i
